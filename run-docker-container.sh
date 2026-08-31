3b8ed4ac0dfed117e67c6b1612dbbf00063e4071
nobukoba <kobayash@rcnp.osaka-u.ac.jp>
2026-08-31 22:47:23 +0900

    Bind local work directory into Docker container

--- run-docker-container.sh
+++ run-docker-container.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 IMAGE="${IMAGE:-container-interfacing-nestdaq-eicrecon:latest}"
+WORK_DIR="${WORK_DIR:-$PWD/work}"
+
+mkdir -p "${WORK_DIR}"
+
 docker run --rm -it \
   --name container-interfacing-nestdaq-eicrecon \
   --platform linux/amd64/v2 \
   --network host \
+  -v "${WORK_DIR}:/work" \
   -e DISPLAY=host.docker.internal:0 \
   "${IMAGE}"
